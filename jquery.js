b666bc216e3ba3a045c5b16707f32a73b7b53f23
Ghost151998 <lucky.barhi@gmail.com>
2018-11-05 03:06:14 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,24 @@
+//star rating
+var rating = 4.36;
+const starTotal = 5;
+const starPercentage = (rating/starTotal)*100;
+const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+document.querySelector(".stars-inner").style.width = starPercentageRounded; 
+
+
+
+//progress bar
+    console.clear();
+    var meters = document.querySelectorAll('svg[data-value] .meter');
+    var m;
+    meters.forEach(function (path) {
+      var length = path.getTotalLength();
+      var value = parseInt(path.parentNode.getAttribute('data-value'));
+      if(value<1){
+        m=1;}
+      else{
+        m=10;}
+      var to = length * ((m - value) / m);
+      path.getBoundingClientRect();
+      path.style.strokeDashoffset = Math.max(0, to);
+    });
